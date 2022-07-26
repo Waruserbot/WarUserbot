6c00c3a233f7c1335d90697262d0feb977d40439
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-26 09:29:48 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -36,7 +36,7 @@ class Config(object):
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
     if bool(EXTERNAL_REPO and (EXTERNAL_REPO.lower() != "false")):
         if not url(EXTERNAL_REPO):
-            EXTERNAL_REPO = "Your Value"
+            EXTERNAL_REPO = "https://github.com/xAbhish3k/waruserbot"
     else:
         EXTERNAL_REPO = None
     # if you need badcat plugins use the following vars
